bcf53b536842602b72a75c1a2bf137c2a5c4bbf1
Nelson Gardner-Challis <nelsongc59@gmail.com>
2021-01-01 16:04:07 +1100

    basic layout set

--- 1.calculator/src/App.js
+++ 1.calculator/src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="display">
+        <h4>0</h4>
+      </div>
+      <div className="panel">
+        <div className="row">
+          <div className="calc-button">
+            <button>AC</button>
+          </div>
+          <div className="calc-button">
+            <button>+/-</button>
+          </div>
+          <div className="calc-button">
+            <button>%</button>
+          </div>
+          <div className="calc-button">
+            <button>Divide</button>
+          </div>
+        </div>
+        <div className="row">
+          <div className="calc-button">
+            <button>7</button>
+          </div>
+          <div className="calc-button">
+            <button>8</button>
+          </div>
+          <div className="calc-button">
+            <button>9</button>
+          </div>
+          <div className="calc-button">
+            <button>X</button>
+          </div>
+        </div>
+        <div className="row">
+          <div className="calc-button">
+            <button>4</button>
+          </div>
+          <div className="calc-button">
+            <button>5</button>
+          </div>
+          <div className="calc-button">
+            <button>6</button>
+          </div>
+          <div className="calc-button">
+            <button>-</button>
+          </div>
+        </div>
+        <div className="row">
+          <div className="calc-button">
+            <button>1</button>
+          </div>
+          <div className="calc-button">
+            <button>2</button>
+          </div>
+          <div className="calc-button">
+            <button>3</button>
+          </div>
+          <div className="calc-button">
+            <button>+</button>
+          </div>
+        </div>
+        <div className="row">
+          <div className="calc-button long">
+            <button>0</button>
+          </div>
+          <div className="calc-button">
+            <button>.</button>
+          </div>
+          <div className="calc-button">
+            <button>=</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ notes.md
@@ -0,0 +1,5 @@
+Plan of Attack for project
+
+CSS Grid Layout for the calculator
+
+functions page - for maths sums, use Math object
